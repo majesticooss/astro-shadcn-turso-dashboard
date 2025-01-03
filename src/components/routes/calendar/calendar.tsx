600517f745595f6277ec8f4860c8804d8c27dd1a
Giulio Zanchetta <zank@outlook.it>
2025-01-03 23:55:07 +0100

    Add calendar component and styles
    - Introduced a new calendar component with event handling.
    - Added CSS styles for the calendar layout and elements.
    - Integrated FullCalendar with various plugins and localization support.
    - Set up loading states and error handling for event fetching.

---
+++ src/components/routes/calendar/calendar.tsx
@@ -0,0 +1,70 @@
+import { useToast } from "@/hooks/use-toast";
+import esLocale from "@fullcalendar/core/locales/es";
+import interactionPlugin from "@fullcalendar/interaction";
+import listPlugin from "@fullcalendar/list";
+import FullCalendar from "@fullcalendar/react";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import { useRef } from "react";
+import styles from "./calendar.module.css";
+
+interface CalendarProps {
+	initialDate?: Date;
+	getCalendarEventsFromAPI: () => Promise<any>;
+	setIsLoading: (loading: boolean) => void;
+	handleDateClick: (arg: any) => void;
+	handleEventClick: (arg: any) => void;
+	handleEventResize: (arg: any) => void;
+	handleEventDrop: (arg: any) => void;
+	handleDatesSet: (arg: any) => void;
+}
+
+export function Calendar({
+	initialDate = new Date(),
+	getCalendarEventsFromAPI,
+	setIsLoading,
+	handleDateClick,
+	handleEventClick,
+	handleEventResize,
+	handleEventDrop,
+	handleDatesSet,
+}: CalendarProps) {
+	const { toast } = useToast();
+	const calendarRef = useRef<any>(null);
+
+	return (
+		<div className={styles.calendarWrapper}>
+			<FullCalendar
+				ref={calendarRef}
+				eventSourceFailure={(error) =>
+					toast({
+						variant: "destructive",
+						title: "Error",
+						description: "Error al cargar el calendario",
+					})
+				}
+				events={getCalendarEventsFromAPI}
+				loading={setIsLoading}
+				dateClick={handleDateClick}
+				eventClick={handleEventClick}
+				eventResize={handleEventResize}
+				eventDrop={handleEventDrop}
+				nowIndicator={true}
+				editable={true}
+				allDaySlot={false}
+				slotMinTime="07:00:00"
+				slotMaxTime="23:00:00"
+				initialView="timeGridWeek"
+				headerToolbar={{
+					left: "prev,next today",
+					center: "title",
+					right: "timeGridWeek,listMonth",
+				}}
+				locales={[esLocale]}
+				plugins={[timeGridPlugin, listPlugin, interactionPlugin]}
+				initialDate={initialDate}
+				datesSet={handleDatesSet}
+				viewDidMount={() => calendarRef.current?.getApi().gotoDate(new Date())}
+			/>
+		</div>
+	);
+}
